976ce73127935c86b01e81995f797a40c249eb98
valekar <srinivas.valekar@gmail.com>
2013-02-09 22:23:39 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learnmaze::Application.config.secret_token = '951ff8db5311810c808c3e925f16c95c97d53400b839e09e0b688808dd0a9099ae5f1c7a4a967d949df9acb5918ea7016e54d910e7d019c0c956c4ec365da558'
